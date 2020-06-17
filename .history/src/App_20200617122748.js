2eb5c4866d73fa51f49a4773c56396a8901f1ad0
syawal2kalii <syawal20162016@gmail.com>
2020-06-17 12:33:08 +0800

    8_map_array_benefits

--- .history/src/App_20200617122748.js
+++ .history/src/App_20200617122748.js
@@ -22,10 +22,7 @@ function FotoProduk() {
 function ProdukInfo(props) {
   const {category,title,isDiscount}=props;
   const price=700000;
-  const benefit = ["Tidak kusut terkena air","Bahan lebih halus","Tidak Gerah"];
-  const listBenefits = benefit.map((itembenefit)=>
-    <li>{itembenefit}</li>
-  );
+  const benefit = ["Tidak kusut terkena air","Bahan lebih halus","Tidak Gerah"]
   return(
       <div class="Deskripsi">
         <p class="Cate">{category}</p>
@@ -35,9 +32,6 @@ function ProdukInfo(props) {
         <p class="Info">
         lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
         </p>
-        <ul>
-          <li>{listBenefits}</li>
-        </ul>
         <a onClick={(e)=>TambahCart(title,e)} href="#">Add to Cart</a>
       </div>
   );
